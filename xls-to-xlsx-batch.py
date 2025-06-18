9b6b0f4951f995a24a1a38de2e999b4e7e39eca1
jessicaraee <76750070+jessicaraee@users.noreply.github.com>
2025-06-18 09:35:09 -0400

    Update xls-to-xlsx-batch.py

--- xls-to-xlsx-batch.py
+++ xls-to-xlsx-batch.py
@@ -4,22 +4,22 @@ import os
 import pandas as pd
 
 #Configure files
-SOURCE_FOLDER = 'SOURCE FOLDER' #Update to filepath and name
-OUTPUT_FOLDER = os.path.join(source_folder, 'Converted') #Update to desired new folder name
+INPUT_FOLDER = "/filepath/input_folder" #Update with filepath and name 
+OUTPUT_FILE = "/filepath/output_file.xlsx" #Update with desired filepath and name
 
-def convert_xls_to_xlsx(SOURCE_FOLDER, OUTPUT_FOLDER):
+def convert_xls_to_xlsx(INPUT_FOLDER, OUTPUT_FOLDER):
     #Create output folder
     os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
     # List all .xls files in the directory
-    xls_files = [f for f in os.listdir(SOURCE_FOLDER) if f.endswith('.xls') and not f.startswith('~$')]
+    xls_files = [f for f in os.listdir(INPUT_FOLDER) if f.endswith('.xls') and not f.startswith('~$')]
     
     if not xls_files:
         print("No .xls files found.")
         return
 
     for file in xls_files:
-        xls_path = os.path.join(SOURCE_FOLDER, file)
+        xls_path = os.path.join(INPUT_FOLDER, file)
         xlsx_filename = file.replace('.xls', '.xlsx')
         xlsx_path = os.path.join(OUTPUT_FOLDER, xlsx_filename)
 
@@ -32,7 +32,7 @@ def convert_xls_to_xlsx(SOURCE_FOLDER, OUTPUT_FOLDER):
             print(f"Failed to convert {file}: {e}")
 
 if __name__ == "__main__":  
-    if os.path.isdir(SOURCE_FOLDER):
+    if os.path.isdir(INPUT_FOLDER):
         convert_xls_to_xlsx(SOURCE_FOLDER, OUTPUT_FOLDER)
         print(f"\nConverted files saved in: {OUTPUT_FOLDER}.")
 
